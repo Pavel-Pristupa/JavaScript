31de235120c1c75ca981507ebc60ea6f67f4042b
Pavel-Pristupa <pavelprystupa@gmail.com>
2022-04-22 22:51:21 +0300

    Some minor changes, changing var for let/const

--- JS_HW_1_pro/HW_1_pro.js
+++ JS_HW_1_pro/HW_1_pro.js
@@ -1,89 +1,73 @@
-const minAge = 18;
-const maxAge = 60;
+const MIN_AGE = 18;
+const MAX_AGE = 60;
 
 // Task 1*:
-function checkAge_1(age){
-    if (age < minAge){
-        console.log(`You don't have access cause your age is ${age}. It's less than ${minAge}`)
-    } else if (age >= minAge && age < maxAge) {
-        console.log("Welcome!");
-    } else if (age > maxAge) {
-        console.log("Keep calm and look Culture channel");
-    } else {
-        console.log("Technical work");
-    }
+function checkAge_1(age) {
+	if (age < MIN_AGE) {
+		console.log(
+			`You don't have access cause your age is ${age}. It's less than ${MIN_AGE}`
+		);
+	} else if (age >= MIN_AGE && age < MAX_AGE) {
+		console.log('Welcome!');
+	} else if (age > MAX_AGE) {
+		console.log('Keep calm and look Culture channel');
+	} else {
+		console.log('Technical work');
+	}
 }
 
-checkAge_1(17)
-checkAge_1(18)
-checkAge_1(61)
-
+checkAge_1(17);
+checkAge_1(18);
+checkAge_1(61);
 
 // // Task 2*:
-function checkAge_2(age){
-    if(typeof age === 'number'){
-        if (age < minAge){
-            console.log(`You don't have access cause your age is ${age}. It's less than ${minAge}`)
-        } else if (age >= minAge && age < maxAge) {
-            console.log("Welcome!");
-        } else if (age > maxAge) {
-            console.log("Keep calm and look Culture channel");
-        } else {
-            console.log("Technical work");
-        }
-    } else {
-        console.log(`Error. You've entered "${age}", that's not a number.`)
-    }
+function checkAge_2(age) {
+	if (typeof age === 'number') {
+		if (age < MIN_AGE) {
+			console.log(
+				`You don't have access cause your age is ${age}. It's less than ${MIN_AGE}`
+			);
+		} else if (age >= MIN_AGE && age < MAX_AGE) {
+			console.log('Welcome!');
+		} else if (age > MAX_AGE) {
+			console.log('Keep calm and look Culture channel');
+		} else {
+			console.log('Technical work');
+		}
+	} else {
+		console.log(`Error. You've entered "${age}", that's not a number.`);
+	}
 }
 
-checkAge_2(17)
-checkAge_2(18)
-checkAge_2(61)
-checkAge_2("asd")
-
+checkAge_2(17);
+checkAge_2(18);
+checkAge_2(61);
+checkAge_2('asd');
 
 // // Task 3**:
-function checkAge_3(age){
-    if(typeof age === 'number' || !isNaN(+age)){
-        if (age < minAge){
-            console.log(`You don't have access cause your age is ${age}. It's less than ${minAge}`)
-        } else if (age >= minAge && age < maxAge) {
-            console.log("Welcome!");
-        } else if (age > maxAge) {
-            console.log("Keep calm and look Culture channel");
-        } else {
-            console.log("Technical work");
-        }
-    } else {
-        console.log(`Error. You've entered "${age}", that's not a number.`)
-    }
+function checkAge_3(age) {
+	if (typeof age === 'number' || !isNaN(+age)) {
+		if (age < MIN_AGE) {
+			console.log(
+				`You don't have access cause your age is ${age}. It's less than ${MIN_AGE}`
+			);
+		} else if (age >= MIN_AGE && age < MAX_AGE) {
+			console.log('Welcome!');
+		} else if (age > MAX_AGE) {
+			console.log('Keep calm and look Culture channel');
+		} else {
+			console.log('Technical work');
+		}
+	} else {
+		console.log(`Error. You've entered "${age}", that's not a number.`);
+	}
 }
 
-checkAge_3(17)
-checkAge_3(18)
-checkAge_3(61)
-checkAge_3("asd")
-checkAge_3('2')
+checkAge_3(17);
+checkAge_3(18);
+checkAge_3(61);
+checkAge_3('asd');
+checkAge_3('2');
 
-
-// // Task 4***:
-function checkAge_4(){
-    var age = prompt('What\'s your age?');
-    var minAge = 18;
-    var maxAge = 60;
-    if(typeof age === 'number' || !isNaN(+age)){
-        if (age < minAge){
-            alert(`You don't have access cause your age is ${age}. It's less than ${minAge}`)
-        } else if (age >= minAge && age < maxAge) {
-            alert("Welcome!");
-        } else if (age > maxAge) {
-            alert("Keep calm and look Culture channel");
-        } else {
-            alert("Technical work");
-        }
-    } else {
-        alert(`Error. You've entered "${age}", that's not a number.`)
-    }
-}
-checkAge_4()
-
+// Task 4***:
+// Запустите файл 'index.html'
--- JS_HW_2/HW_2.js
+++ JS_HW_2/HW_2.js
@@ -1,57 +1,69 @@
 function validateString(str) {
-  let strInput = String(str);
-  let numberFlag = false;
-  let charFlag = false;
-  let upperCharFlag = false;
-  let atCharFlag = false;
-  let notEmptyFlag = false;
+	let strInput = String(str);
+	let numberFlag = false;
+	let charFlag = false;
+	let upperCharFlag = false;
+	let atCharFlag = false;
+	let notEmptyFlag = false;
 
-  if(strInput !== ""){
-    notEmptyFlag = true
-  } else {
-    console.log("You've entered an empty string.")
-  };
-  if(/[a-zA-Z]/.test(strInput)){
-    charFlag = true
-  }else {
-    console.log(`Your string "${strInput}" contains no letters.`)
-  };
-  if(/[0-9]/.test(strInput)){
-    numberFlag = true
-  } else {
-    console.log(`Your string "${strInput}" contains no numbers.`)
-  };
-  if(/[A-Z]/.test(strInput)){
-    upperCharFlag = true
-  } else {
-    console.log(`Your string "${strInput}" contains no uppercase letters.`)
-  };
-  if(strInput.includes('@')) {
-    atCharFlag = true
-  } else {
-    console.log(`Your string "${strInput}" contains no '@'.`)
-  };  
-  if (strInput.length < 5){
-    console.log(`Your string "${strInput}" is too SHORT, it's ${strInput.length} characters long but should be at least 5.`)
-  };
-  if(strInput.length > 64) {
-    console.log(`Your string "${strInput}" is too LONG, it's ${strInput.length} characters long but should be not more than 64.`)
-  };
-  if(numberFlag && charFlag && upperCharFlag && atCharFlag && notEmptyFlag && 5 <= strInput.length && strInput.length <= 64){
-    console.log(`Your string "${strInput}" is good to go! Well done!`)
-  }
+	if (strInput !== '') {
+		notEmptyFlag = true;
+	} else {
+		console.log("You've entered an empty string.");
+	}
+	if (/[a-zA-Z]/.test(strInput)) {
+		charFlag = true;
+	} else {
+		console.log(`Your string "${strInput}" contains no letters.`);
+	}
+	if (/[0-9]/.test(strInput)) {
+		numberFlag = true;
+	} else {
+		console.log(`Your string "${strInput}" contains no numbers.`);
+	}
+	if (/[A-Z]/.test(strInput)) {
+		upperCharFlag = true;
+	} else {
+		console.log(`Your string "${strInput}" contains no uppercase letters.`);
+	}
+	if (strInput.includes('@')) {
+		atCharFlag = true;
+	} else {
+		console.log(`Your string "${strInput}" contains no '@'.`);
+	}
+	if (strInput.length < 5) {
+		console.log(
+			`Your string "${strInput}" is too SHORT, it's ${strInput.length} characters long but should be at least 5.`
+		);
+	}
+	if (strInput.length > 64) {
+		console.log(
+			`Your string "${strInput}" is too LONG, it's ${strInput.length} characters long but should be not more than 64.`
+		);
+	}
+	if (
+		numberFlag &&
+		charFlag &&
+		upperCharFlag &&
+		atCharFlag &&
+		notEmptyFlag &&
+		5 <= strInput.length &&
+		strInput.length <= 64
+	) {
+		console.log(`Your string "${strInput}" is good to go! Well done!`);
+	}
 }
 
 // Test data
-var string_1 = '';
-var string_2 = 'aa';
-var string_3 = 'myString';
-var string_4 = 'myStr1ng';
-var string_5 = 'Lorem 1psum dolor sit @met, consectetuer adipiscing elit, sed dia';
-var string_6 = 'my@'
-var string_7 = '@myStr1ng'
-var string_8 = 1111
-var string_9 = 'mY@1'
+const string_1 = '';
+const string_2 = 'aa';
+const string_3 = 'myString';
+const string_4 = 'myStr1ng';
+const string_5 =
+	'Lorem 1psum dolor sit @met, consectetuer adipiscing elit, sed dia';
+const string_6 = 'my@';
+const string_7 = '@myStr1ng';
+const string_8 = 1111;
+const string_9 = 'mY@1';
 
-validateString(string_8)
-
+validateString(string_8);
